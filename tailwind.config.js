a671d2a8d2b951d7e0b020de5f829f1d9203ac24
vustur <102847236+vustur@users.noreply.github.com>
2023-08-23 17:12:00 +0300

    update colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
     content: ["*.{html,js,css}"],
     theme: {
-      extend: {},
+      extend: {
+        colors: {
+          'purploo': '#8043F0',
+          'orangoo': '#fc7c26',
+          'graypurp': '#4B4453',
+          'pinkman': '#cb6df7',
+        },
+      },
     },
     plugins: [],
 }
