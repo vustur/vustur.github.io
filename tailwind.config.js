475814b65a4b165e391351892ab1213b18498d54
Vustur <koldasa26@gmail.com>
2024-10-07 01:41:10 +0300

    403/404 pages, small changes in index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["*.{html,js,css}"],
+    content: ["./**/*.{html,js,css}"],
     theme: {
       extend: {
         colors: {
